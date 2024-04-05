525af4da3219cd24a03468de8368b759aeed4cf5
Thada Wangthammang <thada.wth@gmail.com>
2024-04-05 17:09:23 +0700

    demo

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AzurermProvider } from "@cdktf/provider-azurerm/lib/provider";
+import * as az from '@cdktf/provider-azurerm';
+import { ResourceGroup } from "@cdktf/provider-azurerm/lib/resource-group";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
+
+    new AzurermProvider(this, "AzureRm", {
+      features: {},
+    });
+
+    const resourceGroup = new ResourceGroup(this, "rg-example", {
+      name: "devops-resource-group",
+      location: "eastus",
+    })
+
+    resourceGroup.id
   }
 }
 
